434a84e2c66337857895c811630c6fee772fc88c
Leojung0823 <leo.jung0823@gmail.com>
2026-07-30 07:47:16 +0800

    feat: keep staging out of search indexes

---
+++ src/app/robots.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  if (process.env.APP_ENV !== "production") {
+    return { rules: [{ userAgent: "*", disallow: "/" }] };
+  }
+  return { rules: [{ userAgent: "*", allow: "/" }] };
+}
